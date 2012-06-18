1a245b1c2355cab34ab1e2f9139c36681a262106
Ruan Zeelie <ruan@unomena.com>
2012-06-18 21:07:15 +0200

    Pycon Static Complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Author: Unomena */
+
+	//Show and Show and Hide and Show etc...
+	$(".register").click(function() {
+		$('.pop_register').show();
+	});
+	$(".pop_close").click(function() {
+		$('.pop_register').hide();
+	});
