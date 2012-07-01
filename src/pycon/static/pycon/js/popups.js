c4f49fa54322405279dab56e5c093d3ef20e3abb
Ruan Zeelie <ruan@unomena.com>
2012-07-01 21:20:15 +0200

    Chopped the shit out of colorbox

--- src/pycon/static/pycon/js/popups.js
+++ src/pycon/static/pycon/js/popups.js
@@ -3,14 +3,16 @@
 $(document).ready(function(){
 	
 	var standard_arguments = {
-			target : '#cboxLoadedContent',
-			beforeSubmit : function(){ 
-								$('#popup_overlay').show(); 
-								$('#popup_overlay_loading').show();
-							},
-			success : function(){ $.colorbox.resize(); },
-			clearForm : 'true'
-			}
+		target : '#cboxLoadedContent',
+		beforeSubmit : function(){ 
+			$('#popup_overlay').show(); 
+			$('#popup_overlay_loading').show();
+		},
+		success : function(){ 
+			$.colorbox.resize(); 
+		},
+		clearForm : 'true'
+	}
 
 	$('#frmRegister').ajaxForm(standard_arguments);
 	$('#frmSpeaker').ajaxForm(standard_arguments);
