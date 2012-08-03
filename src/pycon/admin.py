e21a3b38e4fc9aa9d338f4576abc426640f2118a
Sanet Stevens <sanet@unomena.com>
2012-08-03 15:54:05 +0200

    Fixed the admin.

--- src/pycon/admin.py
+++ src/pycon/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from pycon.models import AttendeeRegistration, SpeakerRegistration
 
-admin.site.register(AttendeeRegistration)
-admin.site.register(SpeakerRegistration)
+#==============================================================================
+class AttendeeRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'registration_type', 'timestamp')
+    search_fields = ('name', 'surname')
+    
+admin.site.register(AttendeeRegistration, AttendeeRegistrationAdmin)
+
+#==============================================================================
+class SpeakerRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'timestamp')
+    search_fields = ('name', 'surname')
+    
+admin.site.register(SpeakerRegistration, SpeakerRegistrationAdmin)    
